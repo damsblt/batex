0077d8e9458660d4b853ef638f03ee7e84490c14
Damien Balet <damien@bbs---damien-balet---macbook-1.home>
2025-06-05 21:11:08 +0200

    Ensure HashRouter and home route for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Menuiserie from "./pages/services/Menuiserie";
@@ -26,7 +25,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <div className="min-h-screen bg-neutral-50">
           <Navigation />
           <Routes>
@@ -45,7 +44,7 @@ const App = () => (
           </Routes>
           <Footer />
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
